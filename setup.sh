cc32996bdc8cc75fbf848c0dd619c47c723632c1
Peek426 <peek426@protonmail.com>
2024-11-26 05:47:20 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,10 +2,13 @@
 
 # 更新软件包
 sudo apt update
-
+sudo apt install python3-venv  python3-pip
+sudo apt install python3-dev libmysqlclient-dev
+sudo apt install default-libmysqlclient-dev build-essential pkg-config
 # 安装所需的包
 sudo apt install -y python3-pip python3-tk ttf-mscorefonts-installer imagemagick imagemagick-6.q16 ffmpeg
-
+python3 -m venv myenv # 创建虚拟环境
+source myenv/bin/activate # 激活虚拟环境
 # 安装 Python 库
 pip install telethon
 pip install python-socks[asyncio]
@@ -27,8 +30,8 @@ PYTHON_VERSION=$(python3 -c 'import sys; print(f"{sys.version_info.major}.{sys.v
 
 # 从 GitHub 下载并替换文件
 sudo curl -o /etc/ImageMagick-6/policy.xml https://raw.githubusercontent.com/Peek426/setup/refs/heads/main/policy.xml
-sudo curl -o /usr/local/lib/python${PYTHON_VERSION}/dist-packages/FastTelethonhelper/__init__.py https://raw.githubusercontent.com/Peek426/setup/refs/heads/main/__init__.py
-sudo curl -o /usr/local/lib/python${PYTHON_VERSION}/dist-packages/telethon/client/uploads.py https://raw.githubusercontent.com/Peek426/setup/refs/heads/main/uploads.py
+sudo curl -o ./myenv/lib/python${PYTHON_VERSION}/dist-packages/FastTelethonhelper/__init__.py https://raw.githubusercontent.com/Peek426/setup/refs/heads/main/__init__.py
+sudo curl -o ./myenv/lib/python${PYTHON_VERSION}/dist-packages/telethon/client/uploads.py https://raw.githubusercontent.com/Peek426/setup/refs/heads/main/uploads.py
 sudo curl -o /usr/local/lib/python${PYTHON_VERSION}/dist-packages/moviepy/video/io/ffmpeg_reader.py https://raw.githubusercontent.com/Peek426/setup/refs/heads/main/ffmpeg_reader.py
 
 echo "所有操作已完成！"
